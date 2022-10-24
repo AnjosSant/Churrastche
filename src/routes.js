45c77675e8b090532dd57e226ed393d3fad5f41c
VitorPerrone <vitorperrone2005@gmail.com>
2022-10-24 11:05:08 -0300

    tab bar

--- src/routes.js
+++ src/routes.js
@@ -5,17 +5,88 @@ import Convidados from "./pages/Convidados/convidados.js";
 import Lista from "./pages/Lista/lista.js";
 import Loja from "./pages/Loja/loja.js";
 import Mapa from "./pages/Mapa/mapa.js";
+import Icon from "react-native-vector-icons/FontAwesome";
 
 const Tab = createBottomTabNavigator();
 
 function Routes() {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Convidados" component={Convidados} options={{headerShown:false}}/>
-      <Tab.Screen name="Shop" component={Loja} options={{headerShown:false}}/>
-      <Tab.Screen name="Home" component={Home} options={{headerShown:false}}/>
-      <Tab.Screen name="Mapa" component={Mapa} options={{headerShown:false}}/>
-      <Tab.Screen name="Lista" component={Lista}  options={{headerShown:false}} />
+    <Tab.Navigator
+      screenOptions={{
+        tabBarActiveTintColor: "black",
+        tabBarInactiveTintColor: "#EA1D2C",
+        tabBarStyle: {
+          position: "absolute",
+          backgroundColor: "#FFFFFF",
+          borderTopWidth: 0,
+        },
+      }}
+    >
+      <Tab.Screen
+        name="Convidados"
+        component={Convidados}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ color, size, focused }) => {
+            if (focused) {
+              return <Icon name="users" size={size} color={color} />;
+            }
+            return <Icon name="users" size={size} color={color} />;
+          },
+        }}
+      />
+      <Tab.Screen
+        name="Shop"
+        component={Loja}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ color, size, focused }) => {
+            if (focused) {
+              return <Icon name="store" size={size} color={color} />;
+            }
+            return <Icon name="store" size={size} color={color} />;
+          },
+        }}
+      />
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ color, size, focused }) => {
+            if (focused) {
+              return <Icon name="home" size={size} color={color} />;
+            }
+            return <Icon name="home" size={size} color={color} />;
+          },
+        }}
+      />
+      <Tab.Screen
+        name="Mapa"
+        component={Mapa}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ color, size, focused }) => {
+            if (focused) {
+              return <Icon name="map" size={size} color={color} />;
+            }
+            return <Icon name="map" size={size} color={color} />;
+          },
+        }}
+      />
+      <Tab.Screen
+        name="Lista"
+        component={Lista}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ color, size, focused }) => {
+            if (focused) {
+              return <Icon name="list" size={size} color={color} />;
+            }
+            return <Icon name="list" size={size} color={color} />;
+          },
+        }}
+      />
     </Tab.Navigator>
   );
 }
