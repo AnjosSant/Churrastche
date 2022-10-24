d0b0fbf3f526517dba59b1fa20b7e8c8fbbed75f
VitorPerrone <vitorperrone2005@gmail.com>
2022-10-24 11:21:02 -0300

    lista

--- src/pages/Lista/lista.js
+++ src/pages/Lista/lista.js
@@ -5,8 +5,24 @@ export default function Home(){
     return(
 
     <View style = {styles.container}>
-        <Text style={styles.text}>Pagina lista</Text>
+        <View style = {styles.header}>
+        <Text style={styles.textHeader}>Despesas</Text>
+        </View>
+        <View style={styles.body}>
+        <Text style={styles.textBody}>Despesas com Carnes                       R$: 00,00</Text>   
+        <Text style={styles.textBody}>Despesas com Bebidas                     R$: 00,00</Text>   
+        <Text style={styles.textBody}>Despesas com Outros                       R$: 00,00</Text>   
+        </View>
+        <View style={styles.footer}>
+        <Text style={styles.textFooter}>Total: R$:00,00</Text>           
+        </View>
+        <View style={styles.rateio}>
+        <Text style={styles.textRateio}>Custo por pagante:                       R$: 00,00</Text>  
+        </View>
     </View>
+    
+   
+
     );
 }
 const styles = StyleSheet.create({
@@ -16,8 +32,61 @@ const styles = StyleSheet.create({
         justifyContent: "center",
         alignItems:'center',
     },
-    text:{
-        fontSize: 25,
-        fontWeight: 'bold'
-    }
+    header:
+    {
+        width:'80%',
+        height: '7%',
+        justifyContent:"center",
+        backgroundColor: '#D03838',
+        borderWidth:1.3,
+        padding: 5,
+        
+    },
+    textHeader:
+    {
+        color: '#ffffff',
+        fontWeight: 500,
+    },
+    body:
+    {
+        width:'80%',
+        height:'25%',
+        borderWidth:1.3,
+        padding: 8,
+        backgroundColor: "#ffffff",
+    },
+    textBody:
+    {
+        color: '#000000',
+        fontWeight: 400,
+        justifyContent: 'center',
+    },
+    footer:
+    {
+        width:'80%',
+        height:'7%',
+        borderWidth:1.3,
+        padding: 8,
+        backgroundColor: "#D03838",
+    },
+    textFooter:
+    {
+        color:'#ffffff',
+        fontWeight: 500,
+        justifyContent: 'center',
+    },
+    rateio:{
+        width:'80%',
+        height:'7%',
+        marginTop:30,
+        backgroundColor: "#D03838",  
+        borderWidth:1.3,
+        padding: 8      
+    },
+    textRateio:
+    {
+        color: '#ffffff',
+        fontWeight: 500,
+        justifyContent: 'center',
+    },
 })
